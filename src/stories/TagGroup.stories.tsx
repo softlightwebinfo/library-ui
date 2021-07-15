973e65b4dc5c6d5bd343664287bbf302c5ccfd1c
rafael.gonzalez <rafael.gonzalez@okmobilitygroup.com>
2021-07-15 15:24:56 +0200

    v0.3.0

---
+++ src/stories/TagGroup.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {ButtonGroup} from "../components/Button/ButtonGroup";
+import {Button} from "../components/Button";
+
+export default {
+    title: 'Base/Button/Group',
+    component: ButtonGroup,
+    argTypes: {},
+    args: {}
+} as ComponentMeta<typeof ButtonGroup>;
+
+const Template: ComponentStory<typeof ButtonGroup> = (args) => (
+    <ButtonGroup {...args} >
+        <Button>Default</Button>
+        <Button>Largue</Button>
+        <Button>Small</Button>
+    </ButtonGroup>
+);
+
+export const Default = Template.bind({});
+export const Vertical = Template.bind({});
+Vertical.args = {
+    vertical: true,
+};
