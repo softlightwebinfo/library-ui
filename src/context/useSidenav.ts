5fd754e13d2fd486b45c462cee98bfb825c4234f
rafael.gonzalez <rafael.gonzalez@okmobilitygroup.com>
2021-07-16 12:43:52 +0200

    v0.6.0

---
+++ src/context/useSidenav.ts
@@ -0,0 +1,13 @@
+import {createContext, SyntheticEvent, useContext} from "react";
+import {TNavAppearance} from "../types/TNavAppearance";
+
+export interface IUseNav {
+    reversed?: boolean;
+    vertical?: boolean;
+    appearance?: TNavAppearance,
+    activeKey?: any,
+    onSelect?: (eventKey: any, event: SyntheticEvent<any>) => void,
+}
+
+export const ContextNav = createContext<IUseNav>({})
+export const useNav = () => useContext(ContextNav);
