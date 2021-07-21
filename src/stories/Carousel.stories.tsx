6f82bd2412a3cbe5a4336f2b7bc84230d28c19df
rafael.gonzalez <rafael.gonzalez@okmobilitygroup.com>
2021-07-21 12:50:40 +0200

    v0.19.0

---
+++ src/stories/Carousel.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Button} from "../components/Button/Button";
+import {ButtonToolbar} from "../components/Button/ButtonToolbar";
+
+export default {
+    title: 'Base/Button/Toolbar',
+    component: ButtonToolbar,
+    argTypes: {},
+    args: {}
+} as ComponentMeta<typeof ButtonToolbar>;
+
+const Template: ComponentStory<typeof ButtonToolbar> = (args) => (
+    <ButtonToolbar {...args}>
+        <Button>Default</Button>
+        <Button>Largue</Button>
+        <Button>Small</Button>
+    </ButtonToolbar>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    children: "Default"
+}
