4ef6f30c61fedcbf3225d19714f4201bdb2b123e
Rafael Gonzalez Muñoz <rafaelgonzalezmunoz@MacBook-Pro-de-Rafael.local>
2021-07-22 19:42:19 +0200

    message alert

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,49 +1,51 @@
-import React, {useCallback, useEffect} from "react";
-import styles from '../../sass/modules/Message.module.scss';
-import {IMessageProps} from "../../props/IMessageProps";
+import { useToggle } from "@codeunic/library-hooks";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {messageIcon} from "../../functions";
-import {faTimes} from "@fortawesome/free-solid-svg-icons";
-import {useToggle} from "@codeunic/library-hooks";
+import React, { useCallback, useEffect } from "react";
+import { messageIcon } from "../../functions";
+import { IMessageProps } from "../../props/IMessageProps";
+import styles from '../../sass/modules/Message.module.scss';
 
-export const Message = ({style, type, show = true, closable, className, ...props}: IMessageProps) => {
-    const [open, {toggle, off, on}] = useToggle(show)
-    const cs = classNames(styles.Message, className, {
-        [styles[type ?? 'info']]: type,
-        [styles.ShowIcon]: props.showIcon,
-        [styles.Full]: props.full,
-        [styles.NotTitle]: !props.title && props.showIcon,
-    });
+export const Message = ({ style, type, show = true, closable, isAlert, className, ...props }: IMessageProps) => {
+  const [open, { toggle, off, on }] = useToggle(show);
+  const cs = classNames(styles.Message, className, {
+    [styles[type ?? 'info']]: type,
+    [styles.ShowIcon]: props.showIcon,
+    [styles.Full]: props.full,
+    [styles.NotTitle]: !props.title && props.showIcon,
+    [styles.IsAlert]: isAlert,
+    [styles.Separate]: props.separate,
+  });
 
-    const onClick = useCallback((evt) => {
-        toggle();
-        props.onClose?.(evt);
-    }, [show, closable]);
-    useEffect(() => {
-        show && on();
-        !show && off();
-    }, [show])
-    if (!open) return null;
+  const onClick = useCallback((evt) => {
+    toggle();
+    props.onClose?.(evt);
+  }, [show, closable]);
+  useEffect(() => {
+    show && on();
+    !show && off();
+  }, [show]);
+  if (!open) return null;
 
-    return (
-        <div className={cs} style={style}>
-            <div className={styles.Container}>
-                {closable && (
-                    <button onClick={onClick} type="button" className={styles.Close}>
-                        <FontAwesomeIcon icon={faTimes}/>
-                    </button>
-                )}
-                {props.showIcon && (
-                    <div className={styles.IconWrapper}>
-                        <FontAwesomeIcon icon={messageIcon(type ?? "info")}/>
-                    </div>
-                )}
-                <div className={styles.Content}>
-                    {props.title && <h5 className={styles.Header}>{props.title}</h5>}
-                    <div className={styles.Body}>{props.description}</div>
-                </div>
-            </div>
+  return (
+    <div className={ cs } style={ style }>
+      <div className={ styles.Container }>
+        { closable && (
+          <button onClick={ onClick } type="button" className={ styles.Close }>
+            <FontAwesomeIcon icon={ faTimes }/>
+          </button>
+        ) }
+        { props.showIcon && (
+          <div className={ styles.IconWrapper }>
+            <FontAwesomeIcon icon={ messageIcon(type ?? "info") }/>
+          </div>
+        ) }
+        <div className={ styles.Content }>
+          { props.title && <h5 className={ styles.Header }>{ props.title }</h5> }
+          <div className={ styles.Body }>{ props.description }</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
--- src/props/IMessageProps.ts
+++ src/props/IMessageProps.ts
@@ -1,14 +1,16 @@
-import {IProps} from "../interfaces/IProps";
-import {TTypeBasic} from "../types/TTypeBasic";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
+import { IProps } from "../interfaces/IProps";
+import { TTypeBasic } from "../types/TTypeBasic";
 
 export interface IMessageProps extends IProps {
-    closable?: boolean;
-    show?: boolean;
-    onClose?: (evt: any) => void;
-    description: string | ReactNode;
-    title?: string | ReactNode;
-    type?: TTypeBasic;
-    showIcon?: boolean;
-    full?: boolean;
+  closable?: boolean;
+  separate?: boolean;
+  isAlert?: boolean;
+  show?: boolean;
+  onClose?: (evt: any) => void;
+  description: string | ReactNode;
+  title?: string | ReactNode;
+  type?: TTypeBasic;
+  showIcon?: boolean;
+  full?: boolean;
 }
